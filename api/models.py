1b5631ec4baab0f40f5becd0cdc4b45bd62c09cf
Andre <kurepin.andre@yandex.ru>
2021-01-16 17:59:08 +0300

    v1 models Review-Comments

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
@@ -8,7 +10,7 @@ class Category(models.Model):
 class Title(models.Model):
     name = models.CharField(max_length=50)
     year = models.PositiveSmallIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
@@ -17,3 +19,60 @@ class Genre(models.Model):
 class GenreTitle(models.Model):
     title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
     genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    score_choices = {
+        (0,'0'),
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10'),
+    }
+
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+
+    text = models.TextField()
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    score = models.PositiveSmallIntegerField(
+        choices=score_choices,
+        default=0
+    )
+
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+class Comment(models.Model):
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    text = models.TextField()
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
