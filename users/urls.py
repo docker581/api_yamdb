34f473f8509c27464a8b6091666b5a5b98bd4cb7
Hacher144 <71040128+Hacher144@users.noreply.github.com>
2021-01-21 18:56:49 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from . import views 
 
 urlpatterns = [
-    path('auth/email/', views.EmailAPIView.as_view(), name='email'),
-    path('auth/token/', views.TokenView.as_view(), name='token'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('v1/auth/email/', views.EmailAPIView.as_view(), name='email'),
+    path('v1/auth/token/', views.TokenView.as_view(), name='token'),
+    path('v1/auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
