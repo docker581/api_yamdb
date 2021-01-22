9cd72b0b0c09da99affccf77de4322965fc93a3a
Denis Doktorov <denisd581@gmail.com>
2021-01-22 03:40:36 +0300

    Version 1

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -44,9 +44,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SIMPLE_JWT = {
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views 
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'v1/users', views.UserViewSet, basename='users') 
+router.register(r'v1/users/me', views.UserMeViewSet, basename='users_me')
+
+urlpatterns = router.urls
+urlpatterns += [
     path('v1/auth/email/', views.EmailAPIView.as_view(), name='email'),
     path('v1/auth/token/', views.TokenView.as_view(), name='token'),
-    path('v1/auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
