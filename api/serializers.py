8fdbf0c03a7500080eaf0d16a5968c0061b643eb
Эрнест Ерамян <eeramyam@taxera.ru>
2021-01-24 00:41:03 +0300

    Version 1

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import MISSING_ERROR_MESSAGE
 from rest_framework.validators import UniqueTogetherValidator
 from .models import (
     Category, 
@@ -16,6 +17,15 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        queryset = Genre.objects.all(),
+        slug_field = 'slug',
+        many = True
+    )
+    category = serializers.SlugRelatedField(
+        queryset = Category.objects.all(),
+        slug_field = 'slug'
+    )
     class Meta:
          fields = ('__all__')
          model = Title
