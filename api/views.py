0abe585c405a16273c48e05dd3d9a3d2cbe4c56f
Эрнест Ерамян <eeramyam@taxera.ru>
2021-01-23 16:56:02 +0300

    Version 1

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics, viewsets, status, filters, permissions
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
+from django.utils.text import slugify
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 from django.shortcuts import get_object_or_404
 from .models import Category, Title, Genre, GenreTitle
@@ -9,19 +13,13 @@ from .serializers import CategorySerializer, TitleSerializer, GenreSerializer
 from .permissions import IsSuperuserPermissionOrReadOnly
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(ListModelMixin, CreateModelMixin, DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsSuperuserPermissionOrReadOnly] 
+    permission_classes = [IsSuperuserPermissionOrReadOnly & IsAuthenticatedOrReadOnly] 
     pagination_class = PageNumberPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['name',]
-
-
-    def destroy(self, request, slug):
-        instance = self.get_object(slug=self.kwargs.get('slug'))
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'users',
     'api',
 ]
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
